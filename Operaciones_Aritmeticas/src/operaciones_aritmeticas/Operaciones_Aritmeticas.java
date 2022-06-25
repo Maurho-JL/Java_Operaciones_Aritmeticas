610e1003b4654864098235277eabb92c773a85f3
Maurho <maurhojl@gmail.com>
2022-06-25 16:01:22 -0600

    Primer Commit

---
+++ Operaciones_Aritmeticas/src/operaciones_aritmeticas/Operaciones_Aritmeticas.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operaciones_aritmeticas;
+
+/**
+ *
+ * @author syste
+ */
+public class Operaciones_Aritmeticas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        float suma;
+        float resta;
+        float multiplicacion;
+        float divicion;
+        float v1=10;
+        float v2=5;
+        suma=v1+v2;
+        resta=v1-v2;
+        multiplicacion=v1*v2;
+        divicion = v1/v2;
+        System.out.println("suma: "+suma+"\n"+"resta: "+resta+"\n"+"multiplicacion: "+multiplicacion+"\n"+"divicion: "+divicion);
+                
+    }
+    
+}
